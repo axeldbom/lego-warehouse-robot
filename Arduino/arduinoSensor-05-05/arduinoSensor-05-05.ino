db28e2d5fd4487c302a63e8d180365fff65d13ad
LloydM8082 <49865424+LloydM8082@users.noreply.github.com>
2019-05-06 08:33:07 +0200

    Add files via upload

---
+++ Arduino/arduinoSensor-05-05/arduinoSensor-05-05.ino
@@ -0,0 +1,217 @@
+
+// Include Libraries
+#include "Arduino.h"
+#include "DHT.h"
+#include "Adafruit_SI1145.h"
+#include <Wire.h>
+
+
+
+// Pin Definitions
+#define DHT_PIN_DATA	2
+
+
+
+// Global variables and defines
+
+// object initialization
+HardwareSerial& bthc06(Serial1);
+DHT dht(DHT_PIN_DATA);
+Adafruit_SI1145 uv = Adafruit_SI1145();
+
+
+// define vars for testing menu
+const int timeout = 10000;       //define timeout of 10 sec
+char menuOption = 0;
+long time0;
+String outputString = " ";
+
+// Setup the essentials for your circuit to work. 
+//It runs first every time your circuit is powered with electricity.
+void setup() 
+{
+    // Setup Serial which is useful for debugging
+    // Use the Serial Monitor to view printed messages
+    Serial.begin(9600);
+    while (!Serial) ; // wait for serial port to connect. Needed for native USB
+    Serial.println("start");    
+    //This example uses HC-06 Bluetooth to communicate with an Android device.
+    //Download bluetooth terminal from google play store, 
+    //https://play.google.com/store/apps/details?id=Qwerty.BluetoothTerminal&hl=en
+    //Pair and connect to 'HC-06', the default password for connection is '1234'.
+    //You should see this message from your arduino on your android device
+    bthc06.begin(9600);
+    Serial.println("HC-06 Bluetooth: On");
+    bthc06.println("HC-06 Bluetooth: On");
+    
+    dht.begin();
+    Serial.println("DHT22/11 Humidity and Temperature Sensor: On");
+    
+    if (uv.begin()) {
+      Serial.println("SI1145 Digital UV Index / IR / Visible Light Sensor: On");
+    } else {
+      Serial.println("SI1145 Digital UV Index / IR / Visible Light Sensor: NA");      
+    }
+    menuOption = menu();
+    
+}
+
+// Main logic of your circuit. 
+//It defines the interaction between the components you selected. 
+//After setup, it runs over and over again, in an eternal loop.
+void loop() 
+{
+    if(menuOption == '1') {
+    // HC - 06 Bluetooth Serial Module - Test Code
+    String bthc06Str = "";
+    //Receive String from bluetooth device
+    if (bthc06.available())
+    {
+    //Read a complete line from bluetooth terminal
+    bthc06Str = bthc06.readStringUntil('\n');
+    // Print raw data to serial monitor
+    Serial.print("BT Raw Data: ");
+    Serial.println(bthc06Str);
+    }
+    //Send sensor data to Bluetooth device  
+    bthc06.println("PUT YOUR SENSOR DATA HERE");
+    }
+    else if(menuOption == '2') {
+      displayTempVals();
+    }
+    else if(menuOption == '3')
+    {  
+      displayLightVals();
+    }
+    delay(2000);
+    
+    if (millis() - time0 > timeout)
+    {
+        menuOption = menu();
+    }
+    
+}
+
+
+
+// Menu function for selecting the components to be tested
+// Follow serial monitor for instrcutions
+char menu()
+{
+
+    Serial.println(F("\nWhich component would you like to test?"));
+    Serial.println(F("(1) HC - 06 Bluetooth Serial Module"));
+    Serial.println(F("(2) DHT22/11 Humidity and Temperature Sensor"));
+    Serial.println(F("(3) SI1145 Digital UV Index / IR / Visible Light Sensor"));
+    Serial.println(F("(menu) send anything else or press on board reset button\n"));
+    while (!Serial.available());
+
+    // Read data from serial monitor if received
+    while (Serial.available()) 
+    {
+        char c = Serial.read();
+        if (isAlphaNumeric(c)) 
+        {   
+            
+            if(c == '1') 
+    			Serial.println(F("Now Testing HC - 06 Bluetooth Serial Module"));
+    		else if(c == '2') 
+    			Serial.println(F("Now Testing DHT22/11 Humidity and Temperature Sensor"));
+    		else if(c == '3') 
+    			Serial.println(F("Now Testing SI1145 Digital UV Index / IR / Visible Light Sensor"));
+            else
+            {
+                Serial.println(F("illegal input!"));
+                return 0;
+            }
+            time0 = millis();
+            return c;
+        }
+    }
+}
+
+void displaySensorVals(){
+  displayLightVals();
+  displayTempVals();
+}
+
+void displayLightVals(){
+  float UV = uv.readUV();
+  // the index is multiplied by 100 so to get the
+  // integer index, divide by 100!
+  UV /= 100.0;
+  float VIS = uv.readVisible();
+  float IR = uv.readIR(); 
+
+  outputString = "";
+  outputString += F("UV: ");
+  outputString += String(UV, 2);
+  outputString += F("\tVIS: ");
+  outputString += String(VIS, 0);
+  outputString += F("\tIR: ");
+  outputString += String(IR, 0);
+
+  Serial.println(outputString);
+  bthc06.println(outputString);
+}
+
+void displayTempVals(){
+  // DHT22/11 Humidity and Temperature Sensor - Test Code
+  // Reading humidity in %
+  float dhtHumidity = dht.readHumidity();
+  // Read temperature in Celsius, for Fahrenheit use .readTempF()
+  float dhtTempC = dht.readTempC();
+  outputString = "";
+  outputString += F("Humidity: ");
+  outputString += String(dhtHumidity, 1);
+  outputString += F("%\tTemp: ");
+  outputString += String(dhtTempC, 1);
+  outputString += F("C");
+
+  Serial.println(outputString);
+  bthc06.println(outputString);
+}
+
+/*******************************************************
+
+*    Circuito.io is an automatic generator of schematics and code for off
+*    the shelf hardware combinations.
+
+*    Copyright (C) 2016 Roboplan Technologies Ltd.
+
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*    In addition, and without limitation, to the disclaimers of warranties 
+*    stated above and in the GNU General Public License version 3 (or any 
+*    later version), Roboplan Technologies Ltd. ("Roboplan") offers this 
+*    program subject to the following warranty disclaimers and by using 
+*    this program you acknowledge and agree to the following:
+*    THIS PROGRAM IS PROVIDED ON AN "AS IS" AND "AS AVAILABLE" BASIS, AND 
+*    WITHOUT WARRANTIES OF ANY KIND EITHER EXPRESS OR IMPLIED.  ROBOPLAN 
+*    HEREBY DISCLAIMS ALL WARRANTIES, EXPRESS OR IMPLIED, INCLUDING BUT 
+*    NOT LIMITED TO IMPLIED WARRANTIES OF MERCHANTABILITY, TITLE, FITNESS 
+*    FOR A PARTICULAR PURPOSE, NON-INFRINGEMENT, AND THOSE ARISING BY 
+*    STATUTE OR FROM A COURSE OF DEALING OR USAGE OF TRADE. 
+*    YOUR RELIANCE ON, OR USE OF THIS PROGRAM IS AT YOUR SOLE RISK.
+*    ROBOPLAN DOES NOT GUARANTEE THAT THE PROGRAM WILL BE FREE OF, OR NOT 
+*    SUSCEPTIBLE TO, BUGS, SECURITY BREACHES, OR VIRUSES. ROBOPLAN DOES 
+*    NOT WARRANT THAT YOUR USE OF THE PROGRAM, INCLUDING PURSUANT TO 
+*    SCHEMATICS, INSTRUCTIONS OR RECOMMENDATIONS OF ROBOPLAN, WILL BE SAFE 
+*    FOR PERSONAL USE OR FOR PRODUCTION OR COMMERCIAL USE, WILL NOT 
+*    VIOLATE ANY THIRD PARTY RIGHTS, WILL PROVIDE THE INTENDED OR DESIRED
+*    RESULTS, OR OPERATE AS YOU INTENDED OR AS MAY BE INDICATED BY ROBOPLAN. 
+*    YOU HEREBY WAIVE, AGREE NOT TO ASSERT AGAINST, AND RELEASE ROBOPLAN, 
+*    ITS LICENSORS AND AFFILIATES FROM, ANY CLAIMS IN CONNECTION WITH ANY OF 
+*    THE ABOVE. 
+********************************************************/
