968e8cb112dedb18e3ccbf0404c67474fc0fd7d2
Akram-A <akram_togo@hotmail.com>
2019-05-17 20:23:29 +0200

    update

--- Robot/rpi_integration/ev3Device.py
+++ Robot/rpi_integration/ev3Device.py
@@ -2,12 +2,27 @@ import sys
 sys.path.append('../../')
 import socket
 from Robot.robot_controls import Robot
+import argparse
+
+"""
+Argument that can be passed when running the script
+
+* help: displays all available flags
+* devm: Developer mode, which enables an external window that shows the video feed
+* url: Url to the server, http://<url>
+
+"""
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '--dname', help='Device name - the name of the device to connect to', default='localhost')
+args = parser.parse_args()
+
 
 robot = Robot(30, 30)
 
 
 def server():
-    host = socket.gethostname()   # get local machine name
+    host = args.dname   # get local machine name
     port = 4040  # Make sure it's within the > 1024 $$ <65535 range
 
     s = socket.socket()
