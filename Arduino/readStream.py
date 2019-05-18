0e814453f174c8dbaa12786fd9e0808442cb4069
Geun Ho Lee <ohnu93@gmail.com>
2019-05-18 11:33:10 +0200

    string format changed

--- Arduino/readStream.py
+++ Arduino/readStream.py
@@ -20,23 +20,23 @@ while 1:
         # format string output
         arr = str(stream_line, 'utf-8').strip()[:-1].split(":")
         if init_time==-1:
-            init_time = int(round(time.time() * 1000)) - int(arr[0])
-        arr[0] = str(init_time + int(arr[0]))
+            init_time = int(round(time.time() * 1000)) - int(arr[0][:-3])
+        arr[0] = str(init_time + int(arr[0][:-3]))
         for idx,val in enumerate(arr):
             print_str.append(string_arr[idx] + val)
         if warning:
             warning = False
-        print("{:16}{:10}{:10}{:10}{:10}C{:>16}%".format(print_str[0],print_str[1],print_str[2],print_str[3],print_str[4],print_str[5]))
+        print("{:16}{:10}{:14}{:12}{:>12}C{:>18}%".format(print_str[0],print_str[1],print_str[2],print_str[3],print_str[4],print_str[5]))
     else:
         stripped = str(stream_line, 'utf-8').strip()
         if stripped.startswith("WARNINGS"):
             warning = True
             print(stripped)
             arr = str(ser.readline(), 'utf-8').strip()[:-1].split(":")
-            arr[0] = str(init_time + int(arr[0]))
+            arr[0] = str(init_time + int(arr[0][:-3]))
             for idx,val in enumerate(arr):
                 print_str.append(string_arr[idx] + val)
-        print("{:16}{:10}{:10}{:10}{:10}C{:>16}%".format(print_str[0],print_str[1],print_str[2],print_str[3],print_str[4],print_str[5]))
+        print("{:16}{:10}{:14}{:12}{:>12}C{:>18}%".format(print_str[0],print_str[1],print_str[2],print_str[3],print_str[4],print_str[5]))
 
     if warning == False:
         try:
